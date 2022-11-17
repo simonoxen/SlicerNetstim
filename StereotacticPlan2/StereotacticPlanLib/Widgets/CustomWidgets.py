0141d7149e216efef74fb4e5bb191d15b751d673
Simon Oxenford <oxenfordsimon@gmail.com>
2022-11-17 11:52:56 +0100

    enh custom widget coord system used

--- StereotacticPlan2/StereotacticPlanLib/Widgets/CustomWidgets.py
+++ StereotacticPlan2/StereotacticPlanLib/Widgets/CustomWidgets.py
@@ -8,10 +8,14 @@ class myCoordinatesWidget(ctk.ctkCoordinatesWidget):
     def __init__(self, auxMarkupsNode, name):
         super().__init__()
 
-        systemComboBox = qt.QComboBox(self)
-        systemComboBox.addItems(['RAS', 'XYZ'])
-        systemComboBox.connect('currentTextChanged(QString)', self.onSystemChanged)
-        self.layout().addWidget(systemComboBox)
+        self._updatingCoordinatesFromMarkups = False
+        self._updatingMarkupsFromCoordinates = False
+        self._wasXYZ = False
+
+        self.systemComboBox = qt.QComboBox(self)
+        self.systemComboBox.addItems(['RAS', 'XYZ'])
+        self.systemComboBox.connect('currentTextChanged(QString)', self.onSystemChanged)
+        self.layout().addWidget(self.systemComboBox)
 
         viewAction = qt.QAction(self)
         viewAction.setIcon(qt.QIcon(":/Icons/Small/SlicerVisible.png"))
@@ -20,17 +24,17 @@ class myCoordinatesWidget(ctk.ctkCoordinatesWidget):
         viewButton = qt.QToolButton(self)
         viewButton.setDefaultAction(viewAction)
         viewButton.setToolButtonStyle(qt.Qt.ToolButtonIconOnly)
-        viewButton.setFixedHeight(systemComboBox.height)
+        viewButton.setFixedHeight(self.systemComboBox.height)
         self.layout().addWidget(viewButton)
 
         placeAction = qt.QAction(self)
         placeAction.setIcon(qt.QIcon(":/Icons/Small/SlicerVisible.png"))
         placeAction.setCheckable(True)
-        placeAction.connect("toggled(bool)", self.onPlaceClicked)
+        placeAction.connect("toggled(bool)", self.onPlaceToggled)
         placeButton = qt.QToolButton(self)
         placeButton.setDefaultAction(placeAction)
         placeButton.setToolButtonStyle(qt.Qt.ToolButtonIconOnly)
-        placeButton.setFixedHeight(systemComboBox.height)
+        placeButton.setFixedHeight(self.systemComboBox.height)
         self.layout().addWidget(placeButton)
 
         self.markupsNode = auxMarkupsNode
@@ -40,19 +44,27 @@ class myCoordinatesWidget(ctk.ctkCoordinatesWidget):
 
         self.markupsNodeControlPointIndex = self.markupsNode.GetNumberOfControlPoints()-1
         self.markupsNode.SetNthControlPointVisibility(self.markupsNodeControlPointIndex, False)
+        self.markupsNode.SetNthControlPointLocked(self.markupsNodeControlPointIndex, True)
 
         self.coordinatesChanged.connect(self.updateMarkupsNodeFromCoordinates)
 
-        self._updatingCoordinatesFromMarkups = False
 
     def updateCoordinatesFromMarkupsNode(self, caller, event):
+        if self._updatingMarkupsFromCoordinates:
+            return
         pos = np.zeros(3)
         self.markupsNode.GetNthControlPointPosition(self.markupsNodeControlPointIndex, pos)
         self.setNumpyCoordinates(pos)
 
     def updateMarkupsNodeFromCoordinates(self):
-        if not self._updatingCoordinatesFromMarkups:
-            self.markupsNode.SetNthControlPointPositionFromArray(0, self.getNumpyCoordinates())
+        if self._updatingCoordinatesFromMarkups:
+            return
+        self._updatingMarkupsFromCoordinates = True
+        coords = self.getNumpyCoordinates()
+        if self.systemComboBox.currentText == 'XYZ':
+            coords = np.dot(self.getFrameXYZToRASTransform(), np.append(coords, 1))[:3]
+        self.markupsNode.SetNthControlPointPositionFromArray(0, coords)
+        self._updatingMarkupsFromCoordinates = False
 
     def onSystemChanged(self, system):
         if system == 'RAS':
@@ -81,13 +93,19 @@ class myCoordinatesWidget(ctk.ctkCoordinatesWidget):
             markupsLogic = slicer.modules.markups.logic()
             markupsLogic.JumpSlicesToNthPointInMarkup(self.markupsNode.GetID(), self.markupsNodeControlPointIndex, True)
 
-    def onPlaceClicked(self, active):
-        self._updatingCoordinatesFromMarkups = active
+    def onPlaceToggled(self, active):
         if active:
+            self._wasXYZ = self.systemComboBox.currentText == 'XYZ'
+            self.systemComboBox.currentText = 'RAS'
             self._updatingCoordinatesFromMarkups = True
+            self.markupsNode.SetNthControlPointLocked(self.markupsNodeControlPointIndex, False)
             self.markupsNode.ResetNthControlPointPosition(self.markupsNodeControlPointIndex)
             interactionNode = slicer.app.applicationLogic().GetInteractionNode()
             selectionNode = slicer.app.applicationLogic().GetSelectionNode()
             selectionNode.SetReferenceActivePlaceNodeClassName("vtkMRMLMarkupsFiducialNode")
             selectionNode.SetActivePlaceNodeID(self.markupsNode.GetID())
             interactionNode.SetCurrentInteractionMode(interactionNode.Place)
+        else:
+            self.markupsNode.SetNthControlPointLocked(self.markupsNodeControlPointIndex, True)
+            self._updatingCoordinatesFromMarkups = False
+            self.systemComboBox.currentText = 'XYZ' if self._wasXYZ else 'RAS'
